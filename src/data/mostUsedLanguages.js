58aaecd5b13915e067a93a915748917dd6e4ea6c
GitHub Action <action@github.com>
2023-09-07 21:05:27 +0000

    update github data

--- src/data/mostUsedLanguages.js
+++ src/data/mostUsedLanguages.js
@@ -1,5 +1,5 @@
 export const mostUsedLanguages = [
-  { name: "TypeScript", totalSize: 776147, color: "#3178c6" },
+  { name: "TypeScript", totalSize: 777428, color: "#3178c6" },
   { name: "Rust", totalSize: 316824, color: "#dea584" },
   { name: "Go", totalSize: 281009, color: "#00ADD8" },
   { name: "Python", totalSize: 209732, color: "#3572A5" },
