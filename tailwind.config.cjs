9cd5fbff89ad2806c44327f318bfee78a9bba37c
ot07 <ot07neoki@gmail.com>
2023-09-19 19:49:02 +0900

    reset color theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,20 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
+    colors: {
+      background: {
+        light: '#ffffff',
+        dark: '#0d1116',
+      },
+      't-primary': {
+        light: '#1f2328',
+        dark: '#e6edf3',
+      },
+      't-secondary': {
+        light: '#656d76',
+        dark: '#7d8590',
+      }
+    },
     listStyleType: {
       none: 'none',
       disc: 'disc',
