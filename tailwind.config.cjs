d2475a253366018ced4551d7b44771e2299e499c
ot07 <ot07neoki@gmail.com>
2023-09-21 00:17:33 +0900

    fix github contribution graph style

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,7 @@ module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     colors: {
+      transparent: 'transparent',
       'color-bg': {
         light: '#ffffff',
         dark: '#0d1116',
@@ -34,6 +35,48 @@ module.exports = {
       'color-badge-bg': {
         light: '#ddf4ff',
         dark: '#388bfd1a'
+      },
+      'color-tooltip': {
+        light: '#ffffff',
+        dark: '#000000',
+      },
+      'color-tooltip-bg': {
+        light: '#242930',
+        dark: '#6e7681',
+      },
+      'color-contribution': {
+        0: {
+          light: '#e5e7eb',
+          dark: '#1f2937',
+        },
+        1: {
+          light: '#bae6fd',
+          dark: '#082f49',
+        },
+        2: {
+          light: '#7dd3fc',
+          dark: '#0c4a6e',
+        },
+        3: {
+          light: '#0ea5e9',
+          dark: '#0284c7',
+        },
+        4: {
+          light: '#0284c7',
+          dark: '#0ea5e9',
+        },
+        5: {
+          light: '#0369a1',
+          dark: '#38bdf8',
+        },
+        6: {
+          light: '#075985',
+          dark: '#7dd3fc',
+        },
+        7: {
+          light: '#0c4a6e',
+          dark: '#bae6fd',
+        },
       },
       'color-sun': '#fb923c',
       'color-moon': '#3b82f6',
