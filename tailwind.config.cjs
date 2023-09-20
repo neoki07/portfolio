7c297e272be8387f68b58ebb791c0d82256405f5
ot07 <ot07neoki@gmail.com>
2023-09-20 22:20:40 +0900

    update styles

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,18 +3,32 @@ module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     colors: {
-      background: {
+      'color-bg': {
         light: '#ffffff',
         dark: '#0d1116',
       },
-      't-primary': {
+      'color-text-primary': {
         light: '#1f2328',
         dark: '#e6edf3',
       },
-      't-secondary': {
+      'color-text-secondary': {
         light: '#656d76',
         dark: '#7d8590',
-      }
+      },
+      'color-border': {
+        light: '#afb8c1',
+        dark: '#484f58',
+      },
+      'color-divider': {
+        light: '#d8dee5',
+        dark: '#21262e',
+      },
+      'color-icon': {
+        light: '#afb8c1',
+        dark: '#7d8590',
+      },
+      'color-sun': '#fb923c',
+      'color-moon': '#3b82f6',
     },
     listStyleType: {
       none: 'none',
