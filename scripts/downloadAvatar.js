e29f2d43aec54d25bbbb8c0d97f7baa1c1cc8b02
ot07 <ot07neoki@gmail.com>
2023-12-08 21:09:22 +0900

    download avatar image

---
+++ scripts/downloadAvatar.js
@@ -0,0 +1,13 @@
+import { writeFileSync } from 'fs'
+import { AVATAR_URL } from '../src/constants/me'
+
+const OUTPUT_PATH = './public/avatar.png'
+
+const main = async () => {
+  const res = await fetch(AVATAR_URL)
+  const arrayBuffer = await res.arrayBuffer()
+  const buffer = Buffer.from(arrayBuffer)
+  writeFileSync(OUTPUT_PATH, buffer)
+}
+
+await main()
