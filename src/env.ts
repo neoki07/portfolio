cce2e5e5a1fff9512c542453d4d8961ec4928162
neoki <ot07neoki@gmail.com>
2024-07-22 21:19:40 +0900

    fix: use import.meta.env for accessing environment variables

--- src/env.ts
+++ src/env.ts
@@ -1,20 +1,20 @@
-if (!process.env.GITHUB_NAME) {
+if (!import.meta.env.GITHUB_NAME) {
   throw new Error('`GITHUB_NAME` is not set')
 }
 
-if (!process.env.GITHUB_USERNAME) {
+if (!import.meta.env.GITHUB_USERNAME) {
   throw new Error('`GITHUB_USERNAME` is not set')
 }
 
-if (!process.env.GITHUB_AVATAR_URL) {
+if (!import.meta.env.GITHUB_AVATAR_URL) {
   throw new Error('`GITHUB_AVATAR_URL` is not set')
 }
 
-if (!process.env.ZENN_URL) {
+if (!import.meta.env.ZENN_URL) {
   throw new Error('`ZENN_URL` is not set')
 }
 
 const { GITHUB_NAME, GITHUB_USERNAME, GITHUB_AVATAR_URL, ZENN_URL } =
-  process.env
+  import.meta.env
 
 export { GITHUB_AVATAR_URL, GITHUB_NAME, GITHUB_USERNAME, ZENN_URL }
